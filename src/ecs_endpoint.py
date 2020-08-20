2c84007c1ce076e0a1c99e000d3e957725dbfafa
swaroophs <swarooph@cognicept.systems>
2020-08-20 22:56:37 +0800

    Fixing code review comments.

--- src/ecs_endpoint.py
+++ src/ecs_endpoint.py
@@ -1,106 +1,110 @@
-from sanic import Sanic, response
-from sanic.response import json, html, text
-from databases import Database
+'''
+This program defines and served API endpoint routes for the ERT and ECS tables.
+'''
+
 import os
+import errno
+
+from sanic import Sanic, response
+from databases import Database
 
 app = Sanic(__name__)
 
-app.static('/static', './static')
 
-app.url_for('static', filename='favicon.ico') == '/static/favicon.ico'
+@app.route("/ping")
+async def get_ping(request):
+    """
+    Test API Route
+    """
 
-# Parsing API Components 
-ecs_api_url = os.environ['ECS_API']
-ecs_api_url = ecs_api_url.replace('/','')
-api_comp = ecs_api_url.split(':')
-api_host = api_comp[1]
-api_port = api_comp[2]
-
-@app.route("/")
-async def main_index(request):
-    template = open("index.html")
-    return html(template.read())
+    return response.json({'data': 'ECS API is Live!'}, status=200)
 
 
-@app.route("/api/ert/getAllData")
-async def get_ert_alldata(request):
-    """ Test API that can be used to quickly see ERT data """
-
-    # Get database
-    db = request.app.config['database']
-
-    # Set query
-    query = """
-            SELECT error_code, robot_model, error_source, error_text
-            FROM error_report;
-            """
-
-    # Retrieve query
-    rows = await db.fetch_all(query=query)
-
-    # Return results
-    return response.json({'status': 200, 'data': jsonify(rows)}, status=200)
-
-
-@app.route("/api/ert/getErrorData")
+@app.route("/ert/error-data")
 async def get_ert_errordata(request):
-    """ Primary API that can be used to retrieve ERT row corresponding to a robot model/error text combo 
-        ERT should be primarily used only in development environment, not production. """
+    """
+    Primary API that can be used to retrieve ERT row corresponding to a
+    robot model/error text combo ERT should be primarily used only in
+    development environment, not production.
 
+    :param RobotModel: Model of the Robot which is querying the database
+    :param ErrorText: Error String that is queried
+    :return result: Matching Row for the queried ErrorText and RobotModel
+    :responses
+    -- 200 : Successful query
+    -- 400 : Insufficient parameters to serve request
+    -- 500 : Database issue
+    """
     # Get database
-    db = request.app.config['database']
+    try:
+        db_instance = request.app.config['database']
+    except Exception as ex:
+        return response.json({'data': 'Database ERROR ' + str(ex)}, status=500)
 
     # Get query parameters
-    query_params = request.args
-    RobotModel = query_params.get('RobotModel')
-    ErrorText = query_params.get('ErrorText')
+    try:
+        query_params = request.args
+        robot_model = query_params.get('RobotModel')
+        error_text = query_params.get('ErrorText')
+
+        if not (robot_model and error_text):
+            raise Exception
+
+    except Exception:
+        return response.json({'data': 'Required API parameters missing'}, status=400)
 
     # Set query
     query = """
             SELECT error_code, cast(error_level as integer) as 'error_level', compounding_flag, error_module, error_source, error_text, error_description, error_resolution
             FROM error_report
-            WHERE (:robot_model LIKE robot_model COLLATE NOCASE) AND (:error_text LIKE error_text COLLATE NOCASE); 
+            WHERE (:robot_model LIKE robot_model COLLATE NOCASE) AND (:error_text LIKE error_text COLLATE NOCASE);
             """
 
-    # Retrieve query
-    rows = await db.fetch_all(query=query, values={"robot_model": RobotModel, "error_text": ErrorText})
-    
-    # Return results
-    return response.json({'status': 200, 'data': jsonify(rows)}, status=200)
+    # Retrieve query and return results
+    try:
+        # Retrieve query
+        rows = await db_instance.fetch_all(query=query,
+                                           values={"robot_model": robot_model,
+                                                   "error_text": error_text})
+        # Return results
+        return response.json({'data': rows_to_list(rows)}, status=200)
+    except Exception as ex:
+        return response.json({'data': 'Database Query ERROR, ' + str(ex)}, status=500)
 
 
-@app.route("/api/ecs/getAllData")
-async def get_ecs_alldata(request):
-    """ Test API that can be used to quickly see ECS data """
-
-    # Get database
-    db = request.app.config['database']
-
-    # Set query
-    query = """
-            SELECT client_error_code, cognicept_error_code, error_module, error_source, error_text
-            FROM error_classification
-            """
-
-    # Retrieve query
-    rows = await db.fetch_all(query=query)
-
-    # Return results
-    return response.json({'status': 200, 'data': jsonify(rows)}, status=200)
-
-
-@app.route("/api/ecs/getErrorData")
+@app.route("/ecs/error-data")
 async def get_ecs_errordata(request):
-    """ Primary API that can be used to retrieve ECS row corresponding to a robot model/error text combo 
-        ECS should be the primary choice in a production environment. """
+    """
+    Primary API that can be used to retrieve ECS row corresponding to
+    a robot model/error text combo ECS should be the primary choice
+    in a production environment.
+
+    :param RobotModel: Model of the Robot which is querying the database
+    :param ErrorText: Error String that is queried
+    :return result: Matching Row for the queried ErrorText and RobotModel
+    :responses
+    -- 200 : Successful query
+    -- 400 : Insufficient parameters to serve request
+    -- 500 : Database issue
+    """
 
     # Get database
-    db = request.app.config['database']
+    try:
+        db_instance = request.app.config['database']
+    except Exception as ex:
+        return response.json({'data': 'Database ERROR ' + str(ex)}, status=500)
 
     # Get query parameters
-    query_params = request.args
-    RobotModel = query_params.get('RobotModel')
-    ErrorText = query_params.get('ErrorText')
+    try:
+        query_params = request.args
+        robot_model = query_params.get('RobotModel')
+        error_text = query_params.get('ErrorText')
+
+        if not (robot_model and error_text):
+            raise Exception
+
+    except Exception:
+        return response.json({'data': 'Required API parameters missing'}, status=400)
 
     # Set query
     query = """
@@ -110,35 +114,53 @@ async def get_ecs_errordata(request):
             WHERE :robot_model LIKE X.robot_model COLLATE NOCASE
             AND :error_text LIKE X.error_text COLLATE NOCASE
             """
-    # Retrieve query
-    rows = await db.fetch_all(query=query, values={"robot_model": RobotModel, "error_text": ErrorText})
 
-    # Return results
-    return response.json({'status': 200, 'data': jsonify(rows)}, status=200)
+    # Retrieve query and return results
+    try:
+        # Retrieve query
+        rows = await db_instance.fetch_all(query=query,
+                                           values={"robot_model": robot_model,
+                                                   "error_text": error_text})
+        # Return results
+        return response.json({'data': rows_to_list(rows)}, status=200)
+    except Exception as ex:
+        return response.json({'data': 'Database Query ERROR, ' + str(ex)}, status=500)
 
 
 @app.listener('before_server_start')
 async def register_db(app, loop):
-    # Create a database connection
-    db_path = os.path.expanduser(os.environ['ECS_DB_LOC'])
-    app.config['database'] = Database(
-        'sqlite:///'+ db_path)
-    await app.config['database'].connect()
+    """
+    Connects to the database
+    """
+    try:
+        db_path = os.path.expanduser(os.environ['ECS_DB_LOC'])
+
+        if not os.path.exists(db_path):
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), db_path)
+
+        app.config['database'] = Database(
+            'sqlite:///' + db_path)
+        await app.config['database'].connect()
+    except Exception as ex:
+        print('Database Connectivity ERROR, ' + str(ex))
 
 
 @app.listener('after_server_stop')
 async def close_connection(app, loop):
-    # Disconnect from database
+    """
+    Disconnects from the database
+    """
     await app.config['database'].disconnect()
 
 
-def jsonify(records):
+def rows_to_list(records):
     """
-    Parse db record response into JSON format
+    Parse db record response into list of dicts for JSON format
     """
     raw_list = []
-    for r in records:
-        items = r.items()
+    for record in records:
+        items = record.items()
         raw_list.append({i[0]: i[1].rstrip() if type(
             i[1]) == str else i[1] for i in items})
 
@@ -151,7 +173,7 @@ def jsonify(records):
             else:
                 row['compounding_flag'] = False
             processed_list.append(row)
-    
+
     # If processed list is empty, no processing was done, just assign raw list
     if not processed_list:
         processed_list = raw_list
@@ -161,8 +183,15 @@ def jsonify(records):
 
 if __name__ == "__main__":
 
+    # Parsing API Components
+    ecs_api_url = (os.environ['ECS_API']
+                   if 'ECS_API' in os.environ else 'http://0.0.0.0:8000')
+
+    # Getting the API port from the URL string
+    api_port = (ecs_api_url.split(':'))[2]
+
     try:
-        app.run(port=api_port,
-                access_log=True, debug=False)
-    except Exception as e:
-        print('Exception message: ', e)
+        app.run(host='0.0.0.0', port=api_port,
+                access_log=True, debug=True)
+    except Exception as ex:
+        print('Exception message: ', str(ex))
--- test/test_api.py
+++ test/test_api.py
@@ -1,11 +1,13 @@
 import json
 import requests
 
+# Simple Interactive Test to test the API routes
+
 # Test case - ERROR
-errorText = "Aborting because the robot appears to be oscillating over and over. Even after executing all recovery behaviors"
+# errorText = "Aborting because the robot appears to be oscillating over and over. Even after executing all recovery behaviors"
 
 # Test case - WARN Turtlebot
-# errorText = "Rotation cmd in collision"
+errorText = "Rotation cmd in collision"
 
 # Test case - Info Turtlebot
 # errorText = "Got new plan"
@@ -21,31 +23,27 @@ errorText = "Aborting because the robot appears to be oscillating over and over.
 # errorText = "got new plan"
 # errorText = "GOT NEW PLAN"
 
-print(errorText)
 robotModel = "Turtlebot3"
 query_params = {'RobotModel': robotModel, 'ErrorText': errorText}
-error_response = requests.get('http://localhost:8000' + '/api/ert/getErrorData/', params = query_params)
+print('API Query Parameters: ', query_params)
+
+print('API PING RESPONSE')
+error_response = requests.get('http://localhost:8000' + '/ping')
 print(error_response)
 error_resp_content = json.loads(error_response.content)
 error_data = error_resp_content['data']
+print(error_data)
 
 print('ERT RESPONSE')
-if (len(error_data) > 0):
-    error_info = error_data[0]
-    ecc = error_info['error_level']
-    print(ecc)
-else:
-    print('Normal operation resumes.')
-
-print('ECS RESPONSE')
-error_response = requests.get('http://localhost:8000' + '/api/ecs/getErrorData/', params = query_params)
+error_response = requests.get('http://localhost:8000' + '/ert/error-data/', params = query_params)
 print(error_response)
 error_resp_content = json.loads(error_response.content)
 error_data = error_resp_content['data']
+print(error_data)
 
-if (len(error_data) > 0):
-    error_info = error_data[0]
-    ecc = error_info['severity']
-    print(ecc)
-else:
-    print('Normal operation resumes.')
+print('ECS RESPONSE')
+error_response = requests.get('http://localhost:8000' + '/ecs/error-data/', params = query_params)
+print(error_response)
+error_resp_content = json.loads(error_response.content)
+error_data = error_resp_content['data']
+print(error_data)
